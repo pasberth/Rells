ce73e468cf252268b7c75245d49a99b51f7acbfc
pasberth <pasberth@gmail.com>
2012-03-10 23:20:33 +0900

    Use belld_dynamic_create_a

--- lib/bells/runtime/macro/func.rb
+++ lib/bells/runtime/macro/func.rb
@@ -16,9 +16,9 @@ class Bells::Runtime::Macro::Func
   def bells_eval *args
     e = ->(node) do
       case node
-      when Bells::Syntax::Node::Symbol then (bells_env.dynamic_context || bells_env.receiver).bells_create_a(Macro::Symbol, node.symbol).bells_eval
-      when Bells::Syntax::Node::String then (bells_env.dynamic_context || bells_env.receiver).bells_create_a(Macro::String, node.string).bells_eval
-      when Bells::Syntax::Node::Integer then (bells_env.dynamic_context || bells_env.receiver).bells_create_a(Macro::Integer, node.integer).bells_eval
+      when Bells::Syntax::Node::Symbol then bells_dynamic_create_a(Macro::Symbol, node.symbol).bells_eval
+      when Bells::Syntax::Node::String then bells_dynamic_create_a(Macro::String, node.string).bells_eval
+      when Bells::Syntax::Node::Integer then bells_dynamic_create_a(Macro::Integer, node.integer).bells_eval
       when Bells::Syntax::Node::Macro
         e.(node.node).bells_eval *node.args
       end
