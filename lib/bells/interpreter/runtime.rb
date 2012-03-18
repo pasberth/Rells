266e1416334174c955fd795a8cd7a1662ee865e1
pasberth <pasberth@gmail.com>
2012-03-18 10:39:00 +0900

    bug fix

--- lib/bells/interpreter/runtime.rb
+++ lib/bells/interpreter/runtime.rb
@@ -11,6 +11,7 @@ module Bells::Runtime
     def bells_eval_io input
       parser = ::Bells::Syntax::Parser.new
       node = parser.parse input
+      node = syntax_node_to_runtime_node(node)
       eval node
     end
   end
